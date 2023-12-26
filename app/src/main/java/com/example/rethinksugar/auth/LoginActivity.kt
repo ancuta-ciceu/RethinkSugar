8298552f050574399bcc2fc238ba29b4a984f03e
ancuta-ciceu <71690721+ancuta-ciceu@users.noreply.github.com>
2023-12-26 18:33:29 +0200

    changes in the UI

--- app/src/main/java/com/example/rethinksugar/auth/LoginActivity.kt
+++ app/src/main/java/com/example/rethinksugar/auth/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.rethinksugar
+package com.example.rethinksugar.auth
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.rethinksugar.WelcomeActivity
 import com.example.rethinksugar.databinding.ActivityLogin2Binding
 import com.google.firebase.auth.FirebaseAuth
 
