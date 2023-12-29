14b6ab38efa1730cc48acf05d695845f322a12d8
ancuta-ciceu <71690721+ancuta-ciceu@users.noreply.github.com>
2023-12-29 17:27:23 +0200

    try with local database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:convert-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.8.9")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
